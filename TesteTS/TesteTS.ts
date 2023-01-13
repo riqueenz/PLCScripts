c1133efb41912cb7a96a6388b5e1db0b7339b8d0
Henrique Enzweiler <henriqueenzweiler@gmail.com>
2023-01-12 21:56:54 -0300

    Create TesteTS.ts

---
+++ TesteTS/TesteTS.ts
@@ -0,0 +1,51 @@
+define display lcd:
+//    2160 model, 10 rows, 20 cols
+
+define keyboard: 4 rows, 4 cols
+
+define version: "1.0"
+
+define analogic input:
+	1   ai1
+	2   ai2
+
+define analogic output:
+	1   ao1
+	2   ao2
+
+define digital input:
+	1   di1
+	2   di2
+
+define digital output:
+	1   do1
+	2   do2
+
+define protocol registers:
+	1   ao1
+	2   ao2
+
+word s
+
+/* ---------------------------------------------------------------------------- */
+MACHINE (main)
+
+STATE 0:
+	$wclra()
+	s = 1
+	goto 1
+
+STATE 1:
+	$wstr("Hello world!")
+	s = 2
+	goto 2
+
+STATE 2:
+	$rkey(0)
+	s = 0
+	goto 0
+
+ENDMACHINE
+
+
+
